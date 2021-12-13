d5eb7439244e085579bb4cfcd4152c0ec0d12a4b
Martin Poul Porthouse <martinporthouse@hotmail.com>
2021-12-13 16:34:05 +0100

    Main Menu tilføjet

---
+++ 1. semesterprojekt - Sortering/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene("10 Special test 1");
+    }
+
+    public void StartTurorial()
+    {
+        SceneManager.LoadScene("Turorial");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
